3b524b3998fe868b1183b29049ff30c2e3164660
wenbo <18629531510@163.com>
2021-05-13 17:31:48 +0800

    -

--- core/init.js
+++ core/init.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router')
+const cors = require('koa2-cors')
 const { get } = require('lodash')
 const requireDirectory = require('require-directory')
 
@@ -19,6 +20,18 @@ class InitManager {
                 InitManager.app.use(route.routes()).use(route.allowedMethods())
             }
         }
+    }
+
+    static initCors() {
+        InitManager.app.use(
+            cors({
+                maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+                credentials: true, //是否允许发送Cookie
+                allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+                allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+                exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+            })
+        )
     }
 
     static initLoadError() {
